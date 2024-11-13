f4947e90b5e86fcdf96b25e98d703e29b73141cb
RAHARISON John <raharisonjohn@gmail.com>
2024-11-13 17:29:32 +0100

    Add files via upload

---
+++ Messagerie (C client:serveur)/includes/client.h
@@ -0,0 +1,19 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <string.h>
+
+#include "protocole.h"
+
+
+#endif
